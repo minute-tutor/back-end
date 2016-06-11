cb4e2f66f358c37237c1a44a78c0ef66bbc4f7f1
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-06-11 14:24:59 -0700

    Added getUser

--- routes.js
+++ routes.js
@@ -1,9 +1,16 @@
-module.exports = function(app) {
-  app.get('/user/*', function(req, res) {
+module.exports = function (app) {
+  app.get('/user/*', function (req, res) { // /user/token/
     var url = req.url;
     var token = url.substr(url.lastIndexOf("/") + 1, url.length);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ token : token }));
-  })
+    var user = require('./models/user');
+    user.findOne({'google.token': token}).exec(function (err, user) {
+      res.setHeader('Content-Type', 'application/json');
+      if (!err) {
+        res.send(JSON.stringify({user: user}));
+      } else {
+        res.send(JSON.stringify({user: null}))
+      }
+    })
+  });
 };
