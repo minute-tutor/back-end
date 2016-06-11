0c8157ee6f73d307f7e461795f0cf2e389a4b072
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-06-11 11:20:45 -0700

    Initial code

---
+++ app.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var program = require('commander');
+
+program
+  .option('-e --extIP <extIP>', 'External IP to listen on', String)
+  .option('-d --dbIP <dbIP>', 'IP address of mongodb server', String)
+  .option('-p --port <port>', 'Port to listen on', Number, process.env.PORT || 8080);
+program.parse(process.argv);
+
+
+var mongo = require('mongodb');
+mongo.connect(program.dbIP, startListening);
+
+var db = null;
+var studentData = null;
+
+var app = express();
+
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+function startListening(err, db) {
+
+  if (err) {
+    // Print error to console
+    return console.dir(err);
+  } else {
+    // Assign db to what we get
+    this.db = db;
+    db.createCollection('studentData', function (err, collection) {
+      if (!err) {
+        console.log("Created collection studentData!");
+
+        studentData = collection;
+
+        app.listen(program.port, program.extIP);
+        console.log("The good stuff lives on port: " + program.port);
+      } else {
+        console.log("Error in creating studentData collection");
+      }
+      require('./routes/studentPost.js')(app, studentData);
+    });
+  }
+}
